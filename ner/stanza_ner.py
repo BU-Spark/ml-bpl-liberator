3ec854879cf908b9201cb949519c90c7d12e2cba
alihus <alihusain120@gmail.com>
2021-04-26 19:39:25 -0400

    Shortened NER tags

--- ner/stanza_ner.py
+++ ner/stanza_ner.py
@@ -12,6 +12,7 @@ class StanzaNER:
 
     def __init__(self):
         self.nlp = stanza.Pipeline('en', dir=definitions.STANZA_RESOURCES_DIR)
+        self.tags = {"GPE", "LOC", "PERSON", "NORP", "ORG"}
 
 
     def run_ner(self, ocr_text):
@@ -22,11 +23,12 @@ class StanzaNER:
 
         for sent in doc.sentences:
             for ent in sent.ents:
-                if ent.type in ner_dict:
-                    if not ent.text in ner_dict[ent.type]:
-                        ner_dict[ent.type].append(ent.text)
-                else:
-                    ner_dict[ent.type] = [ent.text]
+                if ent.type in self.tags:
+                    if ent.type in ner_dict:
+                        if not ent.text in ner_dict[ent.type]:
+                            ner_dict[ent.type].append(ent.text)
+                    else:
+                        ner_dict[ent.type] = [ent.text]
 
         return ner_dict
 
